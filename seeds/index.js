1aa556450856a793715a20cebb29516b024dc5c5
Naman Soni <namantsoni@gmail.com>
2023-06-12 19:02:17 +0530

    CRUD, Styles and Errors

---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers')
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() *1000);
+        const price = Math.floor(Math.random() * 20) + 10;  
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/190727/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum itaque explicabo aut aliquid in ea repellendus inventore nemo optio enim quidem, incidunt cupiditate ex? Nisi laudantium vel est inventore.',
+            price
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
